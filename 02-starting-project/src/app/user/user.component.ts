23e1ada1e4674378e67fc6b3bf19985f750b0848
Steffan Troxel <sjtroxel@alumni.iu.edu>
2025-03-12 19:29:49 -0500

    inputs and outputs, oh my

--- 02-starting-project/src/app/user/user.component.ts
+++ 02-starting-project/src/app/user/user.component.ts
@@ -1,8 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -12,16 +8,18 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
 
 
-  // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser.avatar
-  // }
+
+get imagePath() {
+  return 'assets/users/' + this.avatar;
+}
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    this.select.emit(this.id);
   }
 }
